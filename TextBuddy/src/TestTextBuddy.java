d9fd095849296af4354ea2906d155c6bb9ca5acd
Tricia Tan <tricia.tzy@gmail.com>
2015-02-28 22:07:31 +0800

    CE2 updates
    forgot to commit after each change. So here’re 94 changes committed in
    one go

---
+++ TextBuddy/src/TestTextBuddy.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestTextBuddy {
+	
+	TextBuddy textbuddy = new TextBuddy();
+
+	// test to 
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
